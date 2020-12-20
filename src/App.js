f51df0ed8f83ccfd970e0c3aa27c27ed27804b38
algerieno <aymen.algerieno2014@gmail.com>
2020-12-20 17:12:12 +0100

    get data from server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useDataLayerValue } from './utils/DataLayer';
+import axios from 'axios'
+
 
 function App() {
+
+  const [{ name }, dispatch] = useDataLayerValue()
+
+  console.log(name);
+
+  
+
+  useEffect(() => {
+    try {
+      axios.get('api/getname').then(response => {
+        dispatch({
+          type: "SET_NAME",
+          payload: response.data
+        })
+      }).catch(error => {
+        if (error.response.status === 404) {
+          console.log("api was not found");
+        }
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [])
+  
+
+
   return (
     <div className="App">
-      <h1>questa è la seconda volta, vabbè?</h1>
+      <h1>look what I'm getting from server {`=>`} {name} :) </h1>
     </div>
   );
 }
---
+++ src/utils/reducer.js
@@ -0,0 +1,19 @@
+export const initialState = {
+    name: ''
+}
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SET_NAME":
+            return {
+                ...state,
+                name: action.payload
+            }
+
+        default: return state
+    }
+}
+
+
+export default reducer
