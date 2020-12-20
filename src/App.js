9e7e5a8e9d4df8a6f3a4dc224b0c2c98023b1323
algerieno <aymen.algerieno2014@gmail.com>
2020-12-20 23:03:19 +0100

    fixed server adding

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { useDataLayerValue } from './utils/DataLayer';
 import axios from 'axios'
-import CircularProgress from '@material-ui/core/CircularProgress';
+import { CircularProgress, TextField, Button } from '@material-ui/core';
 
 
 function App() {
@@ -10,10 +10,17 @@ function App() {
 
   const server = "https://whispering-basin-81140.herokuapp.com"
   const [{ name }, dispatch] = useDataLayerValue()
+  const [userField, setUserField] = useState('')
 
-  console.log(name);
-
-  
+  const addUser = async () => {
+    try {
+      const response = await axios.post(`${server}/users/create`, {name: userField})
+      console.log(response);  
+    } catch (error) {
+      console.log(error.reponse);
+    }
+    
+  }
 
   useEffect(() => {
     try {
@@ -41,6 +48,27 @@ function App() {
       
       <h1>look what I'm getting from server :) </h1>
       <h1>{name ? name : <CircularProgress />}</h1> 
+      <hr/>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        <TextField label="user name" 
+                  variant="outlined" 
+                  value={userField}
+                  onChange={(e) => setUserField(e.target.value)} 
+                  />
+        <Button variant="contained" 
+                color="primary" 
+                style={{margin: '20px 0'}}
+                onClick={addUser}
+                >Add User</Button>
+      </div>
+      <hr/>
+      <div className="listUsers" style={{display: 'flex', flexDirection: 'column'}}>
+      <h3>List of users here</h3>
+      <span>user 1</span>
+      <span>user 1</span>
+      <span>user 1</span>
+      </div>
+      
     </div>
   );
 }
